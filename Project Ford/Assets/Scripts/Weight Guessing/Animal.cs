fdb89701fe29e807d3c7019b1904ff44f5703f9a
Grant <gred.roberts@gmail.com>
2021-11-19 21:04:33 +1100

    Added weight guessing scripts based on prototype

---
+++ Project Ford/Assets/Scripts/Weight Guessing/Animal.cs
@@ -0,0 +1,21 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+[CreateAssetMenu(fileName = "Animal", menuName = "Animals")]
+public class Animal : ScriptableObject
+{
+	[SerializeField] private int _weight = 0;
+
+	[SerializeField] private Sprite _shapeSprite = null;
+
+	public int GetWeight()
+	{
+		return _weight;
+	}
+
+	public Sprite GetSprite()
+	{
+		return _shapeSprite;
+	}
+}
